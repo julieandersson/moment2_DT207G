111f0e6532bbdd906146fb8ad2571305fcbe3804
julieandersson <juan2301@student.miun.se>
2024-09-23 09:07:57 +0200

    inkluderar postgre och anslutning till databasen i server.js

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
+// Inkluderar postgre
+const { Client } = require("pg");
 // Inkluderar express
 const express = require("express");
+const app = express();
+
 // Inkluderar cors för att tillåta alla domäner
 const cors = require("cors");
+// Använder cors
+app.use(cors());
+
+const port = process.env.DB_PORT || 3000;
 
 // Hämtar in variabler från env-fil
 require('dotenv').config();
-
-const app = express();
-const port = process.env.DB_PORT || 3000;
-
-// Använder cors
-app.use(cors());
 // Använder middleware för att automatiskt konvertera till json
 app.use(express.json());
+
+// Anslut till databasen
+const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+});
+
+client.connect((err) => {
+    if(err) {
+        console.log("Anslutning misslyckades." + err);
+    } else {
+        console.log("Anslutningen lyckades.");
+    }
+});
 
 
 // Startar applikation
